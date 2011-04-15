412a478d31559d3d08a9bdd6652a0337686c425e
Melih Onvural <melih.onvural@gmail.com>
2011-04-14 22:43:52 -0700

    Launch into production

--- vendor/plugins/opentok/lib/open_tok.rb
+++ vendor/plugins/opentok/lib/open_tok.rb
@@ -22,9 +22,8 @@ Net::HTTP.version_1_2 # to make sure version 1.2 is used
 
 module OpenTok
   VERSION = "tbrb-v0.91.2011-02-22"
-  API_URL = "https://staging.tokbox.com/hl"
-  #Uncomment this line when you launch your app
-  #API_URL = "https://api.opentok.com/hl";
+  #API_URL = "https://staging.tokbox.com/hl"
+  API_URL = "https://api.opentok.com/hl";
 end
 
 require 'OpenTok/Exceptions'
